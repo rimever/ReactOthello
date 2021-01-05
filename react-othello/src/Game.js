4eb4426db47d375aab7290a41364b0add4516ab4
rimever <mot_siva_ordin@hotmail.com>
2021-01-05 23:08:31 +0900

    code CheckReverseLogic

--- react-othello/src/Game.js
+++ react-othello/src/Game.js
@@ -7,7 +7,7 @@ import Board from './Board';
  * @returns {string|null} 判定が出れば結果を文字列で返します。勝者が決まらない場合は、nullを返します
  */
 function calculateWinner(squares) {
-    var sumDictionary = {};
+    let sumDictionary = {};
     for (let i = 0; i < squares.length; i++) {
         var value = squares[i];
         if (value) {
@@ -20,19 +20,25 @@ function calculateWinner(squares) {
             return null;
         }
     }
-    var result = "";
-    var keys = Object.keys(sumDictionary);
-    if (sumDictionary[keys[0]] > sumDictionary[keys[1]]) {
+    let keys = Object.keys(sumDictionary);
+    if (sumDictionary[keys[0]] === sumDictionary[keys[1]]) {
+        return "Draw";
+    }
+    let result = "";
+    let isWinnerIndex0 = sumDictionary[keys[0]] > sumDictionary[keys[1]];
+    if (isWinnerIndex0) {
         result += `Winner:${keys[0]}`;
     }else {
         result += `Winner:${keys[1]}`;
     }
-    result += " - ";
-    for(var key of keys) {
-        result += ` ${key}:${sumDictionary[key]}`;
-    }
+    let winnerKey = keys[isWinnerIndex0 ? 0 : 1];
+    let loserKey = keys[isWinnerIndex0 ? 1 : 0];
+
+    result += ` [ ${winnerKey} ${sumDictionary[winnerKey]} - ${sumDictionary[loserKey]} ${loserKey} ] `;
     return result;
 }
+
+
 
 class Game extends React.Component {
     constructor(){
@@ -65,7 +71,8 @@ class Game extends React.Component {
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        console.log("reverse:" + (this.checkReverse(squares, i, this.getNextPiece()) ? "yes" : "no"));
+        squares[i] = this.getNextPiece();
         this.setState({
             history: history.concat([{
                 squares: squares
@@ -75,6 +82,133 @@ class Game extends React.Component {
         });
     }
 
+    checkReverse(squares, i, nextPiece) {
+        let baseX = this.getXFromIndex(i);
+        let baseY = this.getYFromIndex(i);
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+                if (x === 0 && y === 0) { continue; }
+                console.log(`${squares}, ${baseX}, ${baseY}, ${nextPiece}, ${x}, ${y}`);
+                if (this.checkReverseInternal(squares, baseX, baseY, nextPiece, x, y)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    checkReverseInternal(squares, baseX, baseY, nextPiece, proceedX, proceedY) {
+        // まず隣が違う駒でないとNG
+        let neighbourX = baseX + proceedX;
+        let neighbourY = baseY + proceedY;
+        if (!this.inBoard(neighbourX,neighbourY)) {
+            return false;
+        }
+        let neighbourPiece = this.getPieceByXY(squares, neighbourX, neighbourY);
+        if (!neighbourPiece || neighbourPiece === nextPiece) {
+            return false;
+        }
+
+        let x = neighbourX + proceedX;
+        let y = neighbourY + proceedY;
+        while (this.inBoard(x,y)) {
+            console.log(`${x} ${y}`);
+            let seekPiece = this.getPieceByXY(squares, x, y);
+            if (!seekPiece) {
+                return false;
+            }
+            if (seekPiece === nextPiece) {
+                return true;
+            }
+
+            x += proceedX;
+            y += proceedY;
+        }
+        return false;
+    }
+
+    /**
+     * X,Y座標で駒を取得します
+     * @param square : 駒の一次元配列
+     * @param x
+     * @param y
+     */
+    getPieceByXY(square, x, y) {
+        return square[this.getIndexFromXY(x, y)];
+    }
+
+    /**
+     * 座標が盤面内にあるかを判定します。
+     * @param x
+     * @param y
+     * @returns {boolean}
+     */
+    inBoard(x, y) {
+        return this.inBoardX(x)
+            && this.inBoardY(y);
+    }
+
+    /**
+     * Y座標が盤面内にあるかを判定します。
+     * @param y
+     * @returns {boolean}
+     */
+    inBoardY(y) {
+        return 0 <= y
+            && y < Board.RowNumber;
+    }
+
+    /**
+     * X座標が盤面内にあるかを判定します。
+     * @param x
+     * @returns {boolean}
+     */
+    inBoardX(x) {
+        return 0 <= x
+            && x < Board.ColumnNumber;
+    }
+
+    /**
+     * 次の駒を取得します。
+     * @returns {string}
+     */
+    getNextPiece() {
+        return this.state.xIsNext ? 'X' : 'O';
+    }
+
+    /**
+     * インデックスからX座標を取得します。
+     * @param i
+     * @returns {number}
+     */
+    getXFromIndex(i) {
+        return i % Board.ColumnNumber;
+    }
+
+    /**
+     * インデックスからY座標を取得します。
+     * @param i
+     * @returns {number}
+     */
+    getYFromIndex(i) {
+        return parseInt(i / Board.ColumnNumber);
+    }
+
+    /**
+     * x,y座標からインデックスに変換します。
+     * @param x
+     * @param y
+     * @returns {number}
+     */
+    getIndexFromXY(x,y) {
+        return y * Board.ColumnNumber + x;
+    }
+
+
+    /**
+     * 描画処理です
+     * @returns {JSX.Element}
+     */
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
@@ -83,7 +217,7 @@ class Game extends React.Component {
         if(winner){
             status = winner;
         }else{
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + this.getNextPiece();
         }
         const moves = history.map((step, move) => {
             const desc = move ? 'Move #' + move : 'Game start';
